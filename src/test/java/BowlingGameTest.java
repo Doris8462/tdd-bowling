b5b10bd308b5a1aa4018bc33678a8ecc22634f4e
Doris8462 <liandou123>
2020-08-03 22:42:13 +0800

    [haungyunjie] feat:text_spare

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.PrintStream;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,5 +27,16 @@ public class BowlingGameTest {
         }
         int result=BowlingGame.calculate(frames);
         assertEquals(result,0);
+    }
+
+    @Test
+    void should_input_spare() {
+        FrameScore[] frames=new FrameScore[11];
+        for(int i=0;i<10;i++){
+            frames[i]=new FrameScore(4,6);
+        }
+        frames[10]=new FrameScore(10);
+        int result=BowlingGame.calculate(frames);
+        assertEquals(result,146);
     }
 }
